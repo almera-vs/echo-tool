975f37edcb319548a29bb62e7083a89dc8ee7eea
Rademenes16 <60035016+Rademenes16@users.noreply.github.com>
2021-03-21 16:33:55 +0100

    Added sidebar navigation

--- text.js
+++ text.js
@@ -1,4 +1,5 @@
 const uppercaseEl = document.getElementById('outputText')
+
 function update(el) {
     uppercaseEl.value = el.value.toUpperCase()
 }
@@ -7,6 +8,7 @@ function copy() {
     let textarea = document.getElementById("outputText");
     textarea.select();
     document.execCommand("copy");
-   document.getElementById("button").innerHTML = "Copied!";
-   setTimeout(() => {  return  document.getElementById("button").innerHTML = "Copy"; }, 2000);
+ document.getElementById("popUp").innerHTML = "Copied!"; 
+ document.getElementById("popUp").style = "display: block; position:absolute; left: 50%; transform: translate(-50%, -50%); margin-top:2vh;font-family:Verdana; font-size:18px;padding:10px;text-decoration:none;text-shadow:0px 0px 10px #00ff00; font-family: Gotham Narrow SSm A,Gotham Narrow SSm B,Rubik,Lato,Lucida Grande,Lucida Sans Unicode,Tahoma,Sans-Serif;";
+   setTimeout(() => {  return  document.getElementById("popUp").style.display = "none"; }, 900);
   }
